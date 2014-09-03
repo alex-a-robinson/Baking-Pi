c73bcb9c1517f538828759edc1746a34f5235aea
Alex Robinson <alex@begly.co.uk>
2014-09-03 19:38:10 +0100

    Completed OK03

--- source/main.s
+++ source/main.s
@@ -2,53 +2,24 @@
 .globl _start
 _start:
 
-/* 
-* This command loads the physical address of the GPIO region into r0.
-*/
-ldr r0,=0x20200000
+b main
 
-/*
-* Our register use is as follows:
-* r0=0x20200000	the address of the GPIO region.
-* r1=0x00040000	a number with bits 18-20 set to 001 to put into the GPIO
-*				function select to enable output to GPIO 16. 
-* then
-* r1=0x00010000	a number with bit 16 high, so we can communicate with GPIO 16.
-*/
-mov r1,#1
-lsl r1,#18
+.section .text
+main:
+mov sp,#0x8000
 
-/*
-* Set the GPIO function select.
-*/
-str r1,[r0,#4]
+pinNum .req r0
+pinFunc .req r1
+mov pinNum,#16
+mov pinFunc,#1
+bl SetGpioFunction
+.unreq pinNum
+.unreq pinFunc
 
-/* 
-* Set the 16th bit of r1.
-*/
-mov r1,#1
-lsl r1,#16
-
-/* 
-* Set GPIO 16 to low, causing the LED to turn on.
-* Then wait and set it to high (turning it off) and repeat
-*/
-
-loop$:
-
-mov r2,#0x3F0000
-wait1$:
-sub r2,#1
-cmp r2,#0
-bne wait1$
-
-str r1,[r0,#40]
-
-mov r2,#0x3F0000
-wait2$:
-sub r2,#1
-cmp r2,#0
-bne wait2$
-
-str r1,[r0,#28]
-b loop$
+pinNum .req r0
+pinVal .req r1
+mov pinNum,#16
+mov pinVal,#0
+bl SetGpio
+.unreq pinNum
+.unreq pinVal
