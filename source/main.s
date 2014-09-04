493a70e3764b60e1f397ab287bed5021efd31d4a
Alex Robinson <alex@begly.co.uk>
2014-09-04 20:02:22 +0100

    Completed OK05

--- source/main.s
+++ source/main.s
@@ -16,20 +16,33 @@ bl SetGpioFunction
 .unreq pinNum
 .unreq pinFunc
 
+ptrn .req r4
+ldr ptrn,=pattern
+ldr ptrn,[ptrn]
+seq .req r5
+mov seq,#0
+
+mov r1,#1
+lsl r1,seq
+and r1,ptrn
+
 loop$:
-	mov r0,#16
-	mov r1,#0
-	bl SetGpio
 
-	ldr r0,=100000
-	bl Wait
-	
-	mov r0,#16
-	mov r1,#1
-	bl SetGpio
-	
-	ldr r0,=100000
-	bl Wait
+mov r0,#16
+mov r1,#1
+lsl r1,seq
+and r1,ptrn
+bl SetGpio
 
+ldr r0,=250000
+bl Wait
+	
+add seq,#1
+and seq,#0b11111
 b loop$
 
+.section .data
+.align 2
+pattern:
+	.int 0b11111111101010100010001000101010
+	
